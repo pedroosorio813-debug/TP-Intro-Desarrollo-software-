b11863c60a8bcbd02e36c83017fc6635a407f787
longotiziano <longo.tiziano.arg@gmail.com>
2026-03-21 20:10:28 +0000

    finalizado de la estructura principal de directorios base, y creacion del menu de opciones

---
+++ menu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hola, te damos la bienvenida al Ejercicio Practico de Bash\nTe presentamos nuestro menu de opciones:"
+echo "Opcion 1: Crear Entorno"
+echo "Opcion 2: Correr Proceso"
+echo "Opcion 3: Listar alumnos ordenados por numero de padron"
+echo "Opcion 4: Mostrar las 10 notas mas altas"
+echo "Opcion 5: Datos de alumno especifico"
+echo "Opcion 6: Salir"
+
+read -p "Ingrese el numero de la opcion elegida: " opcion_elegida
